fc9fe5a1de52e431ec1a698081a8a9f312b17182
jujuboer990805-create <jujuboer990805@gmail.com>
2026-04-24 18:16:46 +0800

    Update index.go

--- api /index.go
+++ api /index.go
@@ -2,21 +2,24 @@ package handler
 
 import (
 	"net/http"
-	"new-api/common"
-    "new-api/middleware"
-    "new-api/model"
-    "new-api/router"
+	"github.com/QuantumNous/new-api/common"
+	"github.com/QuantumNous/new-api/middleware"
+	"github.com/QuantumNous/new-api/model"
+	"github.com/QuantumNous/new-api/router"
 	"github.com/gin-gonic/gin"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	// 1. 初始化配置和数据库
 	common.SetupLogger()
 	common.InitDatabase()
 	model.InitLogDB()
 	
+	// 2. 设置路由
 	engine := gin.Default()
 	router.SetRouter(engine)
 	middleware.SetRouter(engine)
 	
+	// 3. 处理请求
 	engine.ServeHTTP(w, r)
 }
